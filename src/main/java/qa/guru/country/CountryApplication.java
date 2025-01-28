1dcd74eeb66071984008dc80ca8815f3a915a993
zveda21 <zveda.hyan@gmail.com>
2025-01-28 21:07:05 +0400

    add homework 24 grpc

--- src/main/java/qa/guru/country/CountryApplication.java
+++ src/main/java/qa/guru/country/CountryApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import qa.guru.country.domain.Country;
+import qa.guru.country.domain.graphql.CountryInputGql;
 import qa.guru.country.service.CountryService;
 
 import java.util.Date;
@@ -20,21 +21,20 @@ public class CountryApplication {
     CommandLineRunner testCountries(CountryService countryService) {
         return args -> {
             // Step 1: Add a new country
-            Country country = new Country("Test Country", "TC", new Date());
-            System.out.println("Adding new country...");
-            Country addedCountry = countryService.addCountry(country);
-            System.out.println("Added Country: " + addedCountry.countryName() + ", " + addedCountry.countryCode());
-
-            // Step 2: Update the country name
-            Country newCountry = new Country("Zi Yan","ZY" ,new Date());
-            System.out.println("Updating country...");
-            Country updatedCountry = countryService.updateCountryName("TC", newCountry);
-            System.out.println("Updated Country: " + updatedCountry.countryName());
+//            Country country = new Country("Test Country", "TC", new Date());
+//            System.out.println("Adding new country...");
+//            Country addedCountry = countryService.addCountry(country);
+//            System.out.println("Added Country: " + addedCountry.countryName() + ", " + addedCountry.countryCode());
+//
+//            // Step 2: Update the country name
+//            Country newCountry = new Country("Zi Yan","ZY" ,new Date());
+//            System.out.println("Updating country...");
+//            Country updatedCountry = countryService.updateCountryName("TC", newCountry);
+//            System.out.println("Updated Country: " + updatedCountry.countryName());
 
             // Step 3: Retrieve all countries
             System.out.println("Fetching all countries...");
-            countryService.getAllCountries().forEach(c ->
-                    System.out.println("Country: " + c.countryName() + ", " + c.countryCode()));
+//            System.out.printf("---" +countryService.addGqlCountry(new CountryInputGql("ZCountry","ZC")));
         };
     }
 }
